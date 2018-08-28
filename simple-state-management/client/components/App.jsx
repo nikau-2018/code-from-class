beaf45e868b15bbc4c8fb25fe96199c18cabba86
Rich Churcher <rich.churcher@gmail.com>
2018-08-28 17:33:39 +1200

    Add comments

--- simple-state-management/client/components/App.jsx
+++ simple-state-management/client/components/App.jsx
@@ -3,6 +3,25 @@ import React from 'react'
 import Pixel from './Pixel'
 
 const App = () => {
+  // This is fairly new syntax. It just means: "Create a new array with ten thousand elements, and
+  // put a `<Pixel />` component in each element". This also demonstrates that React can accept an
+  // array of components instead of one div, so it's as if we did:
+  //
+  //   return [
+  //     <Pixel />,
+  //     <Pixel />,
+  //     <Pixel />
+  //   ]
+  //
+  // instead of:
+  //
+  //   return (
+  //     <div>
+  //       <Pixel />
+  //       <Pixel />
+  //       <Pixel />
+  //     </div>
+  //   )
   return Array.from({ length: 10000 }, () => <Pixel />)
 }
 
